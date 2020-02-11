062c6a7280364f0c53e5ab3a9bedb9bf99466b72
Harangus Robert <potatogolden76@gmail.com>
2020-02-11 18:23:01 +0200

    V0.1_1
    First dev. build of V0.1
    
    -initial project comit
    -player movement
    -camera setup

---
+++ modular-survival/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+   
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        movement.Normalize();
+
+        movement *= speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+    }
+}
